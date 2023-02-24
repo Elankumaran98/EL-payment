ca52ef767756325e4cdc545b5c465f323b96a9f2
Elankumaran98 <siva98kumarane@gmail.com>
2023-02-24 11:45:51 +0530

    updated

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,14 +1,67 @@
-import React,{useState} from 'react'
-import {CardElement,useStripe, useElements, PaymentElement} from '@stripe/react-stripe-js';
-import axios from 'axios'
+import React, { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import axios from "axios";
 
-function PaymentForm() {
-    const [sucess,setSucess]=useState(false)
-    const stripe=useStripe()
-    const elements=useElements()
-  return (
-    <div>PaymentForm</div>
-  )
+const CARD_OPTIONS = {
+    iconStyle:"solid",
+    style:{
+        base:{
+            iconColor:"#c4f0ff",
+            color:"#fff",
+            fontWeight:"500",
+        }
+    }
 }
 
-export default PaymentForm
+function PaymentForm() {
+  const [sucess, setSucess] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const [error, paymentMethod] = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+
+    if (!error) {
+      try {
+        const { id } = paymentMethod;
+        const response = await axios.path("http://localhost:4000/payment", {
+          amount: 1000,
+          id,
+        });
+        if (response.data.sucess) {
+          console.log("Success");
+          setSucess(true);
+        }
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    } else {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      {!sucess ? (
+        <form onSubmit={handleSubmit}>
+          <fieldset className="FormGroup">
+            <div className="FormRow">
+              <CardElement options={CARD_OPTIONS} />
+            </div>
+          </fieldset>
+          <button>Pay</button>
+        </form>
+      ) : (
+        <div>
+          <h2>Hello</h2>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PaymentForm;
